31848af015b05c275409229be99d7eb7a7d16858
jordanrowe <jordanneilrowe@live.co.uk>
2022-05-08 18:25:11 +0100

    feat: add menu to choose game type

---
+++ src/routes/game/index.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import OxCell from '$lib/components/ox-cell.svelte';
+	import { writable } from 'svelte/store';
+	import type { OxCellState } from '$lib/data/ox-cell-state';
+	import { OxGame } from '$lib/data/ox-game';
+
+	const ox = writable(new OxGame());
+
+	function handleCellClicked(event: CustomEvent<OxCellState>) {
+		ox.update(function (value) {
+			value.place(event.detail.location);
+			return value;
+		});
+	}
+
+	function handleNewGameClicked() {
+		ox.update(function (value) {
+			value.reset();
+			return value;
+		});
+	}
+</script>
+
+<div class="ox-container">
+	<div class="ox-slate">
+		{#each $ox.board.cells() as cell}
+			<OxCell state={cell} on:cellClick={handleCellClicked} />
+		{/each}
+	</div>
+
+	<h2>
+		{#if $ox.isWon()}
+			{$ox.player} wins!
+		{:else if !$ox.hasMovesRemaining()}
+			Draw! No moves remaining!
+		{:else}
+			Player {$ox.player}'s turn!
+		{/if}
+	</h2>
+	<button on:click={handleNewGameClicked}>New Game</button>
+</div>
+
+<style>
+	.ox-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
+	.ox-slate {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: repeat(3, 1fr);
+		width: 512px;
+		height: 512px;
+		background-image: url('ox_board.png');
+		background-size: contain;
+	}
+</style>
