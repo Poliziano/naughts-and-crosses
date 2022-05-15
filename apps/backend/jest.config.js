52208d287793fe50b6bdc28a850d0e7176c4720d
jordanrowe <jordanneilrowe@live.co.uk>
2022-05-15 16:13:02 +0100

    feat: setup CDK and testing

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -1,8 +1,16 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  preset: "jest-dynalite",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  verbose: true,
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
